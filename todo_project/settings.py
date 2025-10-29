0fe5181f9a559588e52968975431c5762b50fc8c
balajiinfo8 <vinothkumarbalaji150@gmail.com>
2025-10-29 20:41:37 +0530

    Initial commit or your message

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'djangoweb-todo-app.onrender.com'
+]
